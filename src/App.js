ccfc93e63ff5ceee5da2f553e5dd50b88fd7ff92
Nouman159 <noumanarshad15926@gmail.com>
2024-09-05 19:04:53 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from "./Screens/Home/Home";
 import Layout from "./Layout/Layout";
-import Register from './Screens/Register/Register'
+import Register from './Screens/Register/Register';
 import Login from "./Screens/Login/Login";
 
 function App() {
+  const navigate = useNavigate(); // useNavigate for programmatic navigation
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    if (!userId) {
+      navigate('/login');
+    } else {
+      navigate('/');
+    }
+  }, [navigate]);
+
   return (
     <BrowserRouter>
-     <Routes >
-    <Route path="/register" element={<Register />}  /> 
-    <Route path="/login" element={<Login />}  /> 
-
-    </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Layout>
         <Routes>
-
           <Route path="/" element={<Home />} />
-          
-
-
         </Routes>
       </Layout>
-
-
-
-
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
 
